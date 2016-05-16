acbbd39efffbda0577159b45fc78977904c5b55a
Oleksandr Khomenko <o.khomenko1993@gmail.com>
2016-05-16 15:44:10 +0000

    pages created

--- webapp/controller/mainview.controller.js
+++ webapp/controller/mainview.controller.js
@@ -1,10 +1,26 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
-	"use strict";
+			"sap/ui/core/mvc/Controller"
+		], function(Controller) {
+			"use strict";
 
-	return Controller.extend("dummenorangeMM.controller.mainview", {
+			return Controller.extend("dummenorangeMM.controller.mainview", {
 
-	});
-
-});
+					handleNav: function(evt) {
+						var navCon = this.getView().byId("navCon");
+						var target = evt.getSource().data("target");
+						var sPageId = target + "Id";
+						if (sap.ui.getCore().byId(sPageId) !== undefined) {
+							navCon.to(sPageId);
+						} else {
+							var oPage = sap.ui.view({
+								id: sPageId,
+								viewName: "dummenorangeMM.view." + target,
+								type: sap.ui.core.mvc.ViewType.XML
+							});
+							navCon.addPage(oPage);
+							navCon.to(oPage);
+						}
+					}
+				
+			});
+		});
