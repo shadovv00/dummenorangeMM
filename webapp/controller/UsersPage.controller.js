14de8f39c2d9bd376bfbd2f18d9ebc6118dae151
Oleksandr Khomenko <o.khomenko1993@gmail.com>
2016-05-18 12:59:27 +0000

    Add and edit user buttons

--- webapp/controller/UsersPage.controller.js
+++ webapp/controller/UsersPage.controller.js
@@ -40,43 +40,115 @@ sap.ui.define([
 
 			var oModel = new sap.ui.model.json.JSONModel(oData);
 			this.getView().setModel(oModel);
+
+			var oUserModel = new sap.ui.model.json.JSONModel();
+			this.getView().setModel(oUserModel, "user");
 		},
 		onOpenAddUserDialog: function(oEvent) {
-		    
+
 			this._getAddUserDialog().open();
+
+			var oEditButton = sap.ui.getCore().byId("editButtonId");
+			var oAddButton = sap.ui.getCore().byId("addButtonId");
+			var oTitle = sap.ui.getCore().byId("titleId");
+
+			oAddButton.setVisible(true);
+			oEditButton.setVisible(false);
+			oTitle.setText("Add user");
+
+			var oUser = {
+				userCode: null,
+				startPlanningDate: null,
+				numberWeeksAvailability: null,
+				growerProductionPlanPattern: null
+			};
+
+			var oUserModel = this.getView().getModel("user");
+			oUserModel.setData(oUser);
+			oUserModel.refresh();
 		},
 
 		onAddUser: function() {
-			this._getAddUserDialog().close();
+
+			var oUser = this.getView().getModel("user").getData();
+
+			if (oUser.userCode != null && oUser.userCode.trim().length > 0) {
+
+				var oUsersModel = this.getView().getModel();
+				var oUsersData = oUsersModel.getData();
+				var oProductionPlanSelect = sap.ui.getCore().byId("growerProductionSelectId");
+				var sKey = oProductionPlanSelect.getSelectedKey();
+
+				oUser.growerProductionPlanPattern = sKey;
+
+				oUsersData.users.push(oUser);
+				oUsersModel.refresh();
+
+				this._getAddUserDialog().close();
+			} else
+				sap.m.MessageToast.show("Please put user code");
+
 		},
 
 		onEdit: function(oEvent) {
-		
-			
+
 			var sPath = oEvent.getSource().getBindingContext().getPath();
 			var oModel = this.getView().getModel();
 			var oData = oModel.getData();
-			var idx = sPath.substring(sPath.lastIndexOf('/') + 1);
-// 			var newValue = {
-// 			    	userCode: null,
-// 					startPlanningDate: null,
-// 					numberWeeksAvailability: null,
-// 					growerProductionPlanPattern: null
-// 			 };
-        var oUser = oData.users[idx];
-        
-                            
-        console.log(oUser);
-        
-        var oUserModel = new sap.ui.model.json.JSONModel(oUser);
-        this.getView().setModel(oUserModel, "user");
-        
+			this._idx = sPath.substring(sPath.lastIndexOf('/') + 1);
+			// 			var newValue = {
+			// 			    	userCode: null,
+			// 					startPlanningDate: null,
+			// 					numberWeeksAvailability: null,
+			// 					growerProductionPlanPattern: null
+			// 			 };
+			var oUser = oData.users[this._idx];
+
+			console.log(oUser);
+
+			var oUserModel = this.getView().getModel("user");
+			oUserModel.setData(oUser);
+			oUserModel.refresh();
+
 			this._getAddUserDialog().open();
-			 var oSelector = sap.ui.getCore().byId("growerProductionSelectId");	
-			 oSelector.setSelectedKey(oUser.growerProductionPlanPattern);  	
-			 
+			var oSelector = sap.ui.getCore().byId("growerProductionSelectId");
+			oSelector.setSelectedKey(oUser.growerProductionPlanPattern);
+
+			var oEditButton = sap.ui.getCore().byId("editButtonId");
+			var oAddButton = sap.ui.getCore().byId("addButtonId");
+			var oTitle = sap.ui.getCore().byId("titleId");
+
+			oAddButton.setVisible(false);
+			oEditButton.setVisible(true);
+			oTitle.setText("Edit user");
+
+		},
+
+		onEditUser: function(oEvent) {
+			var oUser = this.getView().getModel("user").getData();
+			console.log(oUser);
 			
 			
+
+			if (oUser.userCode != null && oUser.userCode.trim().length > 0) {
+                
+				var oUsersModel = this.getView().getModel();
+				var oUsersData = oUsersModel.getData();
+				var oProductionPlanSelect = sap.ui.getCore().byId("growerProductionSelectId");
+				var sKey = oProductionPlanSelect.getSelectedKey();
+
+				oUser.growerProductionPlanPattern = sKey;
+
+			
+
+				oUsersData[this._idx] = oUser;
+				oUsersModel.refresh();
+
+				this._getAddUserDialog().close();
+
+			} else
+				sap.m.MessageToast.show("Please put user code");
+
 		},
 
 		onDelete: function(oEvent) {
@@ -90,16 +162,16 @@ sap.ui.define([
 			oModel.setData(oData);
 		},
 
-        onClose:function() {
-            this._getAddUserDialog().close();
-        },
+		onClose: function() {
+			this._getAddUserDialog().close();
+		},
 		_getAddUserDialog: function() {
 			if (!this._oAddUserDialog) {
 				this._oAddUserDialog = sap.ui.xmlfragment("dummenorangeMM.fragments.AddUserDialog", this);
 				this.getView().addDependent(this._oAddUserDialog);
 			}
 			return this._oAddUserDialog;
-		},
+		}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
