ade8d8c5d9d1985625335561f2e840b3fb3bba3a
Yurii_Biletskyi <Yurii.Biletskyi@thenextview.nl>
2016-05-19 13:12:38 +0000

    planning type + them start

--- webapp/controller/PlanningTypesAndActivitiesPage.controller.js
+++ webapp/controller/PlanningTypesAndActivitiesPage.controller.js
@@ -5,21 +5,135 @@ sap.ui.define([
 
 	return Controller.extend("dummenorangeMM.controller.PlanningTypesAndActivitiesPage", {
 
+		addPlantsMockup: {
+		    Planning: [
+        		{"id": 1,
+        		"planning_type": "ROOTED",
+        		"language": "Dutch",
+        		"description": "Rooted Chrysantemums",
+				"weeks_before": 2,
+				"weeks_after": 0,
+				deleted:0,
+           		"activities": [{
+                	"activity_type":"ROOT",
+            		"language": "Dutch",
+            		"sort_key": 0,
+            		"description": "Rooting days",
+            		"plan_date": 0,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"PLANT",
+            		"language": "Dutch",
+            		"sort_key": 1,
+            		"description": "PLanted date",
+            		"plan_date": 1,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"VEG",
+            		"language": "Dutch",
+            		"sort_key": 2,
+            		"description": "Vegetative days",
+            		"plan_date": 0,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"REA",
+            		"language": "Dutch",
+            		"sort_key": 3,
+            		"description": "Reaction days",
+            		"plan_date": 0,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"HARV",
+            		"language": "Dutch",
+            		"sort_key": 4,
+            		"description": "Harvest days",
+            		"plan_date": 0,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"WAIT",
+            		"language": "Dutch",
+            		"sort_key": 5,
+            		"description": "Vacant days",
+            		"plan_date": 0,
+            		"wait": 1,
+    				deleted:0
+        		    }]
+        		},
+        		{"id": 2,
+        		"planning_type": "UNROOTED",
+        		"language": "Dutch",
+        		"description": "Unrooted Chrysantemums",
+				"weeks_before": 4,
+				"weeks_after": 0,
+				deleted:0,
+           		"activities": [{
+                	"activity_type":"PLANT",
+            		"language": "Dutch",
+            		"sort_key": 1,
+            		"description": "PLanted date",
+            		"plan_date": 1,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"VEG",
+            		"language": "Dutch",
+            		"sort_key": 2,
+            		"description": "Vegetative days",
+            		"plan_date": 0,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"REA",
+            		"language": "Dutch",
+            		"sort_key": 3,
+            		"description": "Reaction days",
+            		"plan_date": 0,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"HARV",
+            		"language": "Dutch",
+            		"sort_key": 4,
+            		"description": "Harvest days",
+            		"plan_date": 0,
+            		"wait": 0,
+    				deleted:0
+        		    }, {
+                	"activity_type":"WAIT",
+            		"language": "Dutch",
+            		"sort_key": 5,
+            		"description": "Vacant days",
+            		"plan_date": 0,
+            		"wait": 1,
+    				deleted:0
+        		    }]
+        		}		        ]
+
+		},
+
+
+
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf dummenorangeMM.view.PlanningTypesAndActivitiesPage
 		 */
-		//	onInit: function() {
-		//
-		//	},
+			onInit: function() {
+		        var oModel = new sap.ui.model.json.JSONModel(this.addPlantsMockup);
+				this.getView().setModel(oModel,"planningModel");
+			}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
 		 * @memberOf dummenorangeMM.view.PlanningTypesAndActivitiesPage
 		 */
-		//	onBeforeRendering: function() {
+		//	,onBeforeRendering: function() {
 		//
 		//	},
 
