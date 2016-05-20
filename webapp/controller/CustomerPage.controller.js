fae4115a0e75ec501e73742af6310101588c7ce8
Oleksandr Khomenko <o.khomenko1993@gmail.com>
2016-05-20 15:26:15 +0000

    Template done

--- webapp/controller/CustomerPage.controller.js
+++ webapp/controller/CustomerPage.controller.js
@@ -12,19 +12,7 @@ sap.ui.define([
 		 */
 		onInit: function() {
 
-			//  alert("CustoemrInit");
-
-			var aGrowerAdvisorsList = {
-				List: [{
-					name: "Peter"
-				}, {
-					name: "Ted"
-				}]
-			};
-			var oGrowerAdvisorsListModel = new sap.ui.model.json.JSONModel(aGrowerAdvisorsList);
-			this.getView().setModel(oGrowerAdvisorsListModel, "growerAdvisorsList");
-
-			this.oData = {
+		    			this.oData = {
 				growerAdvisors: [{
 					id: 1,
 					name: "Peter",
@@ -97,16 +85,6 @@ sap.ui.define([
 								description: "Links",
 								M2: 75,
 								active: false
-							}, {
-								id: "04",
-								description: "Links",
-								M2: 75,
-								active: true
-							}, {
-								id: "05",
-								description: "Links",
-								M2: 75,
-								active: false
 							}]
 
 						}]
@@ -137,6 +115,41 @@ sap.ui.define([
 								description: "Links",
 								M2: 75,
 								active: true
+							}]
+
+						}]
+					}]
+				}, {
+					id: 2,
+					name: "Ted",
+					role: "Approver",
+					customers: [{
+						id: "001001",
+						growerName: "Van Chomen",
+						address: {
+							street: "Harenwert 48",
+							city: "1000 AA Maasland"
+						},
+						phone: "0612345678",
+						greenHouse: [{
+							id: "001",
+							name: "Van Os Chrysanten C.V.",
+							address: {
+								street: "Harenwert 48",
+								city: "1000 AA Maasland"
+							},
+							defaultM2: 75,
+							phone: "0612345678",
+							bays: [{
+								id: "01",
+								description: "Links",
+								M2: 75,
+								active: true
+							}, {
+								id: "02",
+								description: "Links",
+								M2: 75,
+								active: true
 							}, {
 								id: "03",
 								description: "Links",
@@ -154,24 +167,67 @@ sap.ui.define([
 								active: false
 							}]
 
+						}, {
+							id: "002",
+							name: "Van Os Chrysanten C.V.",
+							address: {
+								street: "Aallaan 306",
+								city: "1000 AA Maasland"
+							},
+							defaultM2: 100,
+							phone: "0612345678",
+							bays: [{
+								id: "01",
+								description: "Links",
+								M2: 75,
+								active: true
+							}, {
+								id: "02",
+								description: "Links",
+								M2: 75,
+								active: true
+							}, {
+								id: "03",
+								description: "Links",
+								M2: 75,
+								active: false
+							}]
+
 						}]
 					}]
+					
 				}]
 			};
 
-			var oGrowerAdvisorModel = new sap.ui.model.json.JSONModel(this.oData.growerAdvisors[0]);
-			// 			var oGrowerModel = new sap.ui.model.json.JSONModel(this.oData.growerAdvisors[0].customers[0]);
-			console.log(oGrowerAdvisorModel);
+			var oGrowerAdvisorModel = new sap.ui.model.json.JSONModel(this.oData);
 			this.getView().setModel(oGrowerAdvisorModel, "growAdvisor");
+			
 
+		
 		},
 
-		onCustomerItemSelect: function(oEvent) {
-           
-
-			var sPath = oEvent.getSource().getBindingContext("growAdvisor").getPath();
+		onCustRepresChange: function() {
+		    
+			var sPath = this.getView().byId("growAdvisorSelectorId").getSelectedItem().getBindingContext("growAdvisor").getPath();
 			console.log(sPath);
+			
+			var oCustList = this.getView().byId("customerListId");
+			
+			oCustList.bindElement({
+				path: sPath,
+				model: "growAdvisor"
+			});
+		
+		    oCustList.setSelectedItem(oCustList.getItems()[0]);
+		  this.onCustomerItemSelect();
+			
+			
+		},
 
+		onCustomerItemSelect: function() {
+
+			var sPath = this.getView().byId("customerListId").getSelectedItem().getBindingContext("growAdvisor").getPath();
+			console.log(sPath);
 
 			var oVBox = this.getView().byId("customerDataVBoxId");
 
@@ -181,20 +237,23 @@ sap.ui.define([
 			});
 			console.log(oVBox);
 
+			var oGreenHouseList = this.getView().byId("greenHouseListId");
 
-			var oList = this.getView().byId("greenHouseListId");
-
-			oList.bindObject({
+			oGreenHouseList.bindElement({
 				path: sPath,
 				model: "growAdvisor"
 			});
 
-			console.log(oList);
+			oGreenHouseList.setSelectedItem(oGreenHouseList.getItems()[0]);
+			 this.onGreenHouseSelect();
+			
+		
 		},
 
-		onGreenHouseSelect: function(oEvent) {
-			var sPath = oEvent.getSource().getBindingContext("growAdvisor").getPath();
+		onGreenHouseSelect: function() {
+			var sPath = this.getView().byId("greenHouseListId").getSelectedItem().getBindingContext("growAdvisor").getPath();
 			console.log(sPath);
+	
 
 			var oVBox = this.getView().byId("greenhouseDataVBoxId");
 
@@ -203,7 +262,7 @@ sap.ui.define([
 				model: "growAdvisor"
 			});
 			console.log(oVBox);
-			
+
 			var oTable = this.getView().byId("baysTableId");
 			oTable.bindElement({
 				path: sPath,
@@ -217,8 +276,10 @@ sap.ui.define([
 		 * @memberOf dummenorangeMM.view.CustomerPage
 		 */
 		onBeforeRendering: function() {
-			//alert("CustoemrBefore");
-		},
+		    	var oSelector = this.getView().byId("growAdvisorSelectorId");
+		    	oSelector.setSelectedKey(this.getView().getModel("growAdvisor").getData().growerAdvisors[0].name);
+		    	this.onCustRepresChange();
+		},  
 
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
@@ -226,7 +287,7 @@ sap.ui.define([
 		 * @memberOf dummenorangeMM.view.CustomerPage
 		 */
 		onAfterRendering: function() {
-			//alert("CustoemrAfter");
+
 		}
 
 		/**
